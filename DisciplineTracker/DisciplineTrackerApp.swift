ea11d1cc47aba6dbfcfe1c97c61fa358c4aa2b3d
Balaban Robert-Andrei <balabanrobert2003@gmail.com>
2026-08-22 14:56:03 +0300

    Initial Commit

---
+++ DisciplineTracker/DisciplineTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisciplineTrackerApp.swift
+//  DisciplineTracker
+//
+//  Created by Robert Balaban on 22/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct DisciplineTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
